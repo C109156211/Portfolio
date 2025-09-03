53bdc6f5f00844186efdeac3b8d90d25c6504509
ggttch213 <ggttch213@gmail.com>
2025-09-03 16:29:36 +0800

    index.jsx修改

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import ReactDOM from 'react-dom/client';
 import './index.css'
 import App from './App.jsx'
 
-const root = createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
     <App />
